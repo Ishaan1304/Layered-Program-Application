913716744e02c7df67d5119640569e18336aed54
Ishaan1304 <109105499+Ishaan1304@users.noreply.github.com>
2023-03-10 12:42:36 +0530

    Add files via upload

---
+++ hr/bl/testcases/DesignationManagerDeleteTestCase.java
@@ -0,0 +1,32 @@
+import com.thinking.machines.hr.bl.exceptions.*;
+import com.thinking.machines.hr.bl.managers.*;
+import com.thinking.machines.hr.bl.pojo.*;
+import com.thinking.machines.hr.bl.interfaces.managers.*;
+import com.thinking.machines.hr.bl.interfaces.pojo.*;
+import java.util.*;
+class DesignationManagerDeleteTestCase
+{
+public static void main(String gg[])
+{
+int code=Integer.parseInt(gg[0]);
+DesignationInterface designation=new Designation();
+try
+{
+DesignationManagerInterface designationManager;
+designationManager=DesignationManager.getDesignationManager();
+designationManager.removeDesignation(code);
+System.out.println("Designation removed");
+}catch(BLException blException)
+{
+if(blException.hasGenericException())
+{
+System.out.println(blException.getGenericException());
+}
+List<String> properties=blException.getProperties();
+for(String property:properties)
+{
+System.out.println(blException.getException(property));
+}
+}
+}
+}
---
+++ hr/bl/testcases/EmployeeManagerAddTestCase.java
@@ -0,0 +1,51 @@
+import com.thinking.machines.hr.bl.exceptions.*;
+import com.thinking.machines.hr.bl.managers.*;
+import com.thinking.machines.hr.bl.pojo.*;
+import com.thinking.machines.hr.bl.interfaces.managers.*;
+import com.thinking.machines.hr.bl.interfaces.pojo.*;
+import com.thinking.machines.enums.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+class EmployeeManagerAddTestCase
+{
+public static void main(String gg[])
+{
+try
+{
+String name="Rakesh Gupta";
+DesignationInterface designation=new Designation();
+designation.setCode(3);
+Date dateOfBirth=new Date();
+boolean isIndian=false;
+BigDecimal basicSalary=new BigDecimal("600000");
+String panNumber="A12345";
+String aadharCardNumber="U12345";
+EmployeeInterface employee;
+employee=new Employee();
+employee.setName(name);
+employee.setDesignation(designation);
+employee.setDateOfBirth(dateOfBirth);
+employee.setGender(GENDER.MALE);
+employee.setIsIndian(isIndian);
+employee.setBasicSalary(basicSalary);
+employee.setPANNumber(panNumber);
+employee.setAadharCardNumber(aadharCardNumber);
+EmployeeManagerInterface employeeManager;
+employeeManager=EmployeeManager.getEmployeeManager();
+employeeManager.addEmployee(employee);
+System.out.println("Employee added with employee Id. as:"+employee.getEmployeeId());
+}catch(BLException blException)
+{
+if(blException.hasGenericException())
+{
+System.out.println(blException.getGenericException());
+}
+List<String> properties=blException.getProperties();
+for(String property:properties)
+{
+System.out.println(blException.getException(property));
+}
+}
+}
+}
